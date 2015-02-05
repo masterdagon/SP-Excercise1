97b267e2258c2d379c5fbd733deff22417621b39
masterdagon <Muggi@192.168.0.10>
2015-02-05 20:01:52 +0100

    Added Worlds simplest Web-Server

--- src/sp1/http1/SP1HTTP1.java
+++ src/sp1/http1/SP1HTTP1.java
@@ -4,7 +4,11 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.util.Iterator;
@@ -18,6 +22,7 @@ public class SP1HTTP1 {
 
     static int port = 8080;
     static String ip = "127.0.0.1";
+    static String contentFolder = "public/";
 
     public static void main(String[] args) throws Exception {
         if (args.length == 2) {
@@ -27,6 +32,7 @@ public class SP1HTTP1 {
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
         server.createContext("/headers", new RequestHandlerHeaders());
+        server.createContext("/pages", new RequestHandlerPages());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -85,7 +91,7 @@ public class SP1HTTP1 {
             Iterator it = keySet.iterator();
             System.out.println("keyset Size: " + keySet.size());
             Object[] array = keySet.toArray();
-            
+
             for (Object key : array) {
                 List<String> value = rqh.get(key);
                 sb.append("<tr>\n<td>");
@@ -95,7 +101,7 @@ public class SP1HTTP1 {
                 sb.append("</td>\n</tr>\n");
             }
             sb.append("</table>\n");
-            
+
             sb.append("</body>\n");
             sb.append("</html>\n");
             response = sb.toString();
@@ -104,6 +110,27 @@ public class SP1HTTP1 {
             he.sendResponseHeaders(200, response.length());
             try (PrintWriter pw = new PrintWriter(he.getResponseBody())) {
                 pw.print(response); //What happens if we use a println instead of print --> Explain
+            }
+        }
+    }
+
+    static class RequestHandlerPages implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            File file = new File(contentFolder + "index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+            Headers h = he.getResponseHeaders();
+            h.add("Content-Type", "text/html");
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
             }
         }
     }
